3235fdd51e09d42b860989fcd395d84d8f176fc3
Mira Ali <benamara97amira@gmail.com>
2024-02-11 22:02:14 +0300

    fix bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,18 @@
 import json
 
 
+# List of allowed models
+models = {
+    "BaseModel": "base_model",
+    "Amenity": "amenity",
+    "City": "city",
+    "Place": "place",
+    "Review": "review",
+    "State": "state",
+    "User": "user",
+}
+
+
 class FileStorage:
     """ FileStorage that serializes instances to a JSON file
         and deserializes JSON file to instances
@@ -15,11 +27,16 @@ class FileStorage:
 
     def new(self, obj):
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
 
     def save(self):
+        data = {
+            key: value.to_dict()
+            for key, value
+            in FileStorage.__objects.items()
+        }
         with open(FileStorage.__file_path, "w", encoding="utf-8") as db:
-            json.dump(FileStorage.__objects, db, indent=4)
+            json.dump(data, db, indent=4)
 
     def reload(self):
         try:
@@ -28,6 +45,12 @@ class FileStorage:
                 "r",
                 encoding="utf-8"
             ) as json_file:
-                FileStorage.__objects = json.load(json_file)
+                for obj_id, obj_dict in json.load(json_file).items():
+                    class_name = obj_dict["__class__"]
+                    module_path = f"models.{models[class_name]}"
+                    module = __import__(module_path, fromlist=[class_name])
+                    target_class = getattr(module, obj_dict["__class__"])
+                    obj = target_class(**obj_dict)
+                    FileStorage.__objects[obj_id] = obj
         except FileNotFoundError:
             pass
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -43,7 +43,7 @@ class TestFileStorage(unittest.TestCase):
         self.assertNotEqual(instance.all(), {})
         self.assertEqual(
             instance.all(),
-            {"MockBaseModel.mock_id": {"id": "mock_id"}}
+            {"MockBaseModel.mock_id": mock_base_model}
         )
 
     def test_save_objects(self):
@@ -58,25 +58,41 @@ class TestFileStorage(unittest.TestCase):
             mocked_file().write.assert_called_once_with('{}')
 
     def test_reload_file_not_found(self):
+        """test reload file not found"""
         instance = file_storage.FileStorage()
         with mock.patch('builtins.open', side_effect=FileNotFoundError):
             instance.reload()
             self.assertEqual(instance.all(), {})
 
     def test_reload_success(self):
+        """test reload success"""
+        tested_model = "BaseModel"
+        mock_instance = {
+            f"{tested_model}.e5442077-5777-4920-a253-ef15491f9f34": {
+                "created_at": "2024-02-11T18:41:32.588120",
+                "id": "e5442077-5777-4920-a253-ef15491f9f34",
+                "updated_at": "2024-02-11T18:41:32.588377",
+                "__class__": tested_model
+            }
+        }
         instance = file_storage.FileStorage()
         with mock.patch(
             'builtins.open',
-            mock.mock_open(read_data='{"key": "value"}')
+            mock.mock_open(read_data=str(mock_instance))
         ) as mock_file:
             with mock.patch(
                 'json.load',
-                return_value={"key": "value"}
+                return_value=mock_instance
             ) as mock_json_load:
                 file_storage.FileStorage.reload(self)
                 mock_file.assert_called_once_with(
                     "file.json",
                     "r",
                     encoding="utf-8")
-                self.assertDictEqual(instance.all(), {"key": "value"})
+                self.assertEqual(len(instance.all()), 1)
+                _instance = next(iter(instance.all()))
+                self.assertEqual(
+                    instance.all()[_instance].__class__.__name__,
+                    tested_model
+                )
                 mock_json_load.assert_called_once()
